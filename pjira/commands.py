1a505c8aca13c96a6666258d9942f59415ec5f0f
gabber12 <shubham.sha12@gmail.com>
2017-02-05 17:06:47 +0530

    Added click.echo instead of printadded some colors

--- pjira/commands.py
+++ pjira/commands.py
@@ -34,22 +34,23 @@ def move(issue_key):
 	jra = Jira.get_jira_service();	# Check for exception and ask user to configure
 	issue = jra.get_issue(issue_key)
 	possible_transitions = jra.get_transitions(issue)
-	print map(lambda x: printf(x['id']+" "+x['name']), possible_transitions)
+	map(lambda x: click.echo(x['id']+" "+x['name']), possible_transitions)
 	res = prompt("Please enter the id of transition: ", map(lambda x: x['id'], possible_transitions))
-	print jra.transition_issue(issue, res)
+	click.echo(jra.transition_issue(issue, res))
 
 @cli.command("ls")
 @click.option("--project", "-p", help="Project name")
 @click.option("--assignee", "-s", help ="Filter by assignee")
 @click.option("--all", "-a", is_flag=True, help="Get all issues")
 def list(project, assignee, all):
-	"""Lists Issues"""
-	jra = Jira.get_jira_service();
-	issues = jra.get_issue_for_user(assignee, project, all) # shift list representation logic to IssueMapper
-	issue_reps = map(lambda x:IssueMapper(x).get_short_rep(), issues)
-	if len(issue_reps) == 0:
-		printf("No issue found for you")
-	map(printf, issue_reps)
+    """Lists Issues"""
+    jra = Jira.get_jira_service()
+    issues = jra.get_issue_for_user(assignee, project, all)
+    issue_reps = map(lambda x:IssueMapper(x).get_short_rep(), issues)
+    if len(issue_reps) == 0:
+        click.secho("Seems like you are all done", fg="green")
+    click.echo("Total Issues (%s)"%len(issues))
+    map(click.echo, issue_reps)
 
 def abort_if_false(ctx, param, value):
     if not value:
@@ -61,23 +62,23 @@ def abort_if_false(ctx, param, value):
 @click.option("--summary", "-s", help="Summary")
 @click.option("--desc", "-d" , help="Description", default = "")
 @click.option("--edit_mode", "-e" , is_flag = True)
-@click.option('--yes', is_flag=True, callback=abort_if_false,
-              expose_value=False,
-              prompt='Are you sure you want to create the issue?')
-def create(project, type,summary, desc, edit_mode):
-	"""Creates issue under a project"""
-	if edit_mode:
-		interface =EditorMode('issue')
-		interface.open()
-		data = interface.parse()
-		summary = data['summary']	
-		desc = data['description']
-		if not ask_for_confirmation('Are you sure you want to create issue(Y/N): '):
-			print 'Operation Aborted'
-			return None
-	jra = Jira.get_jira_service();
-	issue = jra.create_issue(project, summary, desc, type)
-	print IssueMapper(issue).get_long_rep()
+@click.option("--yes", "-y" , is_flag = True)
+def create(project, type,summary, desc, edit_mode, yes):
+    """Create a new ticket"""
+    if edit_mode:
+        interface =EditorMode('issue')
+        interface.open()
+        data = interface.parse()
+        summary = data['summary']	
+        desc = data['description']
+
+    if not yes and not click.confirm('Are you sure you want to create issue'):
+        click.secho('Operation Aborted', fg="red")
+        return None
+
+    jra = Jira.get_jira_service()
+    issue = jra.create_issue(project, summary, desc, type)
+    click.echo(IssueMapper(issue).get_long_rep())
 
 @cli.command("comment")
 @click.argument("issue_key")
@@ -85,7 +86,7 @@ def create(project, type,summary, desc, edit_mode):
 def comment(issue_key, comment):
 	"""Creates issue under a project"""
 	jra = Jira.get_jira_service();
-	print jra.add_comment(issue_key, comment)
+	click.echo(jra.add_comment(issue_key, comment))
 
 
 def printf(str):
